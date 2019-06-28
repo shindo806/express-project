0007d5052121207f67ea5d2f8a4509962ed7a584
shindo806 <thaisonlamsp@gmail.com>
2019-06-28 16:44:01 +0700

    fixed login function with 2 ways login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,31 +21,76 @@ module.exports.postLogin = function (req, res) {
         name: email
     }).value();
 
-    if (!userEmail) {
-
-    }
-
-
-
-
-    if (!userEmail && !userName) {
+    if (!userEmail && userName === undefined) {
         res.render('./auth/login', {
             errors: errors = ['Người dùng không tồn tại'],
             values: values = req.body
         })
-        console.log(userEmail.password);
-    } else if (userEmail.password !== password || userName.password !== password) {
+        return;
+    }
+    if (userEmail === undefined && !userName) {
         res.render('./auth/login', {
-            errors: errors = ['Mật khẩu không đúng'],
+            errors: errors = ['Người dùng không tồn tại'],
             values: values = req.body
         })
-
-    } else if (userEmail.password === password) {
-        res.cookie('userId', userEmail.id);
-        res.redirect('./users');
-    } else if (userName.password === password) {
-        res.cookie('userId', userName.id);
-        res.redirect('./users');
+        return;
+    }
+    // Check login status by email or user name
+    if (userName === undefined) {
+        if (userEmail.password !== password) {
+            res.render('./auth/login', {
+                errors: errors = ['Mật khẩu không đúng'],
+                values: values = req.body
+            })
+        }
     }
 
+    if (userEmail === undefined) {
+        if (userName.password !== password) {
+            res.render('./auth/login', {
+                errors: errors = ['Mật khẩu không đúng'],
+                values: values = req.body
+            })
+        }
+    }
+
+    if (userName === undefined) {
+        if (userEmail.password === password) {
+            res.cookie('userID', userEmail.id);
+            res.render('index');
+            return;
+        }
+    }
+    if (userEmail === undefined) {
+        if (userName.password === password) {
+            res.cookie('userID', userName.id);
+            res.render('index');
+            return;
+        }
+    }
 }
+
+
+// if (userEmail.password !== password && userName === undefined) {
+//     res.render('./auth/login', {
+//         errors: errors = ['Mật khẩu không đúng'],
+//         values: values = req.body
+//     })
+//     return;
+// } else if (userEmail.password === password && userName === undefined) {
+//     res.cookie('userID', userEmail.id);
+//     res.render('index');
+//     return;
+// } else if (userEmail === undefined && userName.password !== password) {
+//     res.render('./auth/login', {
+//         errors: errors = ['Mật khẩu không đúng'],
+//         values: values = req.body
+//     })
+//     return;
+// } else if (userEmail === undefined && userName.password === password) {
+//     res.cookie('userID', userName.id);
+//     res.render('index');
+//     return;
+// }
+
+// }
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,12 +1,12 @@
 var db = require('../db');
 
 module.exports.requireAuth = function (req, res, next) {
-    if (!req.cookies.userId) {
+    if (!req.cookies.userID) {
         res.redirect('/auth/login');
         return;
     }
     var user = db.get('users').find({
-        id: req.cookies.userId
+        id: req.cookies.userID
     }).value();
 
     if (!user) {
